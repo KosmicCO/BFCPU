6ad169e09394211597a35d38c94ed53b199bac69
Cruz <cruzbarnum@gmail.com>
2020-01-07 02:04:30 -0800

    Finished outlining the ncurses terminal; however, there are still a few bugs with the terminal: it seems that it does not print anything, which is an issue

--- virtual-machine/bfcpu_interpreter_test.cc
+++ virtual-machine/bfcpu_interpreter_test.cc
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include "non_protocol_print.hh"
+#include "peripheral.hh"
+#include "peripherals/ncurses_keyboard_terminal.hh"
 
 int main(int argc, char ** argv) {
     if(argc != 2) {
@@ -16,19 +18,30 @@ int main(int argc, char ** argv) {
         return 3;
     }
 
+    std::string s;
+    std::cin >> s;
+
+    NCursesKeyboardTerminal * term = NCursesKeyboardTerminal::get_instance();
+    std::shared_ptr<KeyboardInterface> key = term->get_keyboard_peripheral();
+
     std::istream &rom_stream = rom_prog;
     std::vector<Peripheral *> perphs;
-    perphs.push_back(new NonProtocolPrint());
+    perphs.push_back(term);
+    perphs.push_back(key.get());
     BFCPUInterpreter bfi(rom_stream, perphs, 1948);
     rom_prog.close();
 
     while(bfi.next());
     
+    /*
     for(Peripheral * p : perphs) {
         if(p) {
             delete p;
         }
     }
+    */
+
+    perphs.clear();
 
     std::cout << std::endl << "Program completed." << std::endl;
     return 0;
