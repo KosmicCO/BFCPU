1886f21f330cd50d93c25b8581f2766c1e2f88d5
Cruz <cruzbarnum@gmail.com>
2020-08-12 07:59:51 -0700

    cleanup commit

--- bfasm/macro_error_codes.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-const int ERR_IMPROPER_IMPORT_FILE  = 1;
-const int ERR_UNABLE_CLEAR_BFMC     = 2;
-const int ERR_BFM_FILE_NOT_FOUND    = 3;
-const int ERR_IMPROPER_MAIN_FILE    = 4;
